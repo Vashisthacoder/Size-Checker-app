eb9e7db435a457f71c66b2a4c67d448429c15468
kartik vashistha <kartikvashistha08@gmail.com>
2021-12-09 12:18:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const [windowwidth, setWindowwidth] = useState(window.innerWidth)
+
+   useEffect(() => {
+
+    window.addEventListener("resize",updatewindowwidth)
+    
+   }, )
+
+   const updatewindowwidth=()=>{
+     setWindowwidth(window.innerWidth)
+   }
+
+
+
+
+
   return (
-    <div className="App">
-      <h1>hello React.js</h1>
+
+
+    <div className="window">
+      <h1>Window Size Checker </h1>
+     <h2>The Window width => {windowwidth}</h2> 
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
